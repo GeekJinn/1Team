2f55ea3906798881458bca3b7bfc00bc2218906f
GeekJinn <jean71798@gmail.com>
2024-04-02 06:10:57 +0900

    feat: Added functionality to save YOLO-formatted bounding box

--- rerere_txt.py
+++ rerere_txt.py
@@ -1,49 +1,52 @@
 import os
 from PIL import Image
 
-output_dir = 'high'
+def read_bbox_from_txt(txt_file_path):
+    with open(txt_file_path, 'r') as file:
+        lines = file.readlines()
+        bbox = lines[0].strip().split()[1:]  # '0' 다음의 바운딩 박스 좌표 가져오기
+        bbox = [float(coord) for coord in bbox]  # 좌표를 실수형으로 변환
+    return bbox
 
 def convert(size, box):
-    dw = 1./size[0]
-    dh = 1./size[1]
-    x = (box[0] + box[1])/2.0
-    y = (box[2] + box[3])/2.0
-    w = box[1] - box[0]
-    h = box[3] - box[2]
-    x = round(x*dw,6)
-    w = round(w*dw,6)
-    y = round(y*dh,6)
-    h = round(h*dh,6)
-    return (x,y,w,h)
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    x = (box[0] + box[2]) / 2.0
+    y = (box[1] + box[3]) / 2.0
+    w = abs(box[2] - box[0])  # 바운딩 박스의 폭 계산
+    h = abs(box[3] - box[1])  # 바운딩 박스의 높이 계산
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    return (x, y, w, h)
 
-f_input = open('label.txt','r')
-items=f_input.readlines()
-    
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+# 이미지 파일과 바운딩 박스 파일이 있는 디렉토리 경로
+image_dir = 'valid'  
+txt_dir = 'valid (1)/valid (1)' 
 
-for item in items: 
-    data=item.split()
-    #이미지 경로 가져오기
-    img_path=data[0]
-    image = Image.open(img_path[1:-1])
-    width = int(image.size[0])
-    height = int(image.size[1])
-    
-    #이미지 이름 가져오기
-    img=img_path.split('/')
-    file_name=img[1][:-5]
-    
-    #바운딩박스 위치 가져오기
-    xmin = data[1]
-    xmax = data[3]
-    ymin = data[2]
-    ymax = data[4]
-    
-    #바운딩박스 위치 변환하기
-    b = (float(xmin), float(xmax), float(ymin), float(ymax))
-    bb = convert((width, height), b)
-    
-    #변환한거 저장하기
-    f = open(output_dir + file_name + '.txt', 'w')
-    f.write('3' + " " + " ".join([str(a) for a in bb]) + '\n')    
+for file_name in os.listdir(image_dir):
+    if file_name.endswith('.jpg'): 
+        # 이미지 파일 경로
+        image_path = os.path.join(image_dir, file_name)
+        
+        # 바운딩 박스 파일 경로
+        txt_file_name = file_name[:-4] + '.txt'  
+        txt_file_path = os.path.join(txt_dir, txt_file_name)
+        
+        # 바운딩 박스 좌표를 txt 파일에서 읽어오기
+        bbox = read_bbox_from_txt(txt_file_path)
+        
+        # 이미지 열어서 크기 가져오기
+        image = Image.open(image_path)
+        image_size = image.size  # 이미지 크기
+        
+        # 바운딩 박스 좌표를 YOLO 형식으로 변환
+        converted_bbox = convert(image_size, bbox)
+        
+        # 결과를 저장할 파일 경로
+        output_txt_path = os.path.join(txt_dir, file_name[:-4] + '.txt')
+        
+        # 변환된 바운딩 박스 좌표를 0으로 시작하는 포맷으로 수정하여 저장
+        with open(output_txt_path, 'w') as output_file:
+            output_file.write('0 ' + ' '.join([str(coord) for coord in converted_bbox]))
